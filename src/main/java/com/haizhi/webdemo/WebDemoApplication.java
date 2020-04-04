49a44c671d8dfb7358f299797b989d4408dfc5f9
demo10010 <1249515840@qq.com>
2020-04-04 21:56:40 +0800

    集成Knife4j生成接口文档

--- src/main/java/com/haizhi/webdemo/WebDemoApplication.java
+++ src/main/java/com/haizhi/webdemo/WebDemoApplication.java
@@ -1,11 +1,13 @@
 package com.haizhi.webdemo;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableKnife4j
 public class WebDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/haizhi/webdemo/controller/IndexPageController.java
+++ src/main/java/com/haizhi/webdemo/controller/IndexPageController.java
@@ -1,11 +1,18 @@
 package com.haizhi.webdemo.controller;
 
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class IndexPageController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("doc.html#/home");
+    }
 
 }
