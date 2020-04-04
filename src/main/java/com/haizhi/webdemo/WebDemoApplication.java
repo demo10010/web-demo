ffc332b63a397d57c5fc8ccbb49bc5a2aec51a4f
demo10010 <1249515840@qq.com>
2020-04-04 21:48:24 +0800

    web-项目模板集成swagger-ui

---
+++ src/main/java/com/haizhi/webdemo/WebDemoApplication.java
@@ -0,0 +1,15 @@
+package com.haizhi.webdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class WebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/haizhi/webdemo/controller/IndexPageController.java
@@ -0,0 +1,11 @@
+package com.haizhi.webdemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexPageController {
+
+}
