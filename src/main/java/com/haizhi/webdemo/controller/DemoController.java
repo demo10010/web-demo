bba579b27b2da1d351c7897a8c48d70ea7251623
demo10010 <1249515840@qq.com>
2020-05-07 23:04:06 +0800

    cookies 管理

--- src/main/java/com/haizhi/webdemo/controller/DemoController.java
+++ src/main/java/com/haizhi/webdemo/controller/DemoController.java
@@ -8,6 +8,12 @@ import io.swagger.models.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/user")
 public class DemoController {
@@ -20,6 +26,33 @@ public class DemoController {
         return CommonResponse.success(demoService.testMybatis(user));
     }
 
+    @GetMapping("/delCookies")
+    @ApiOperation("测试delCookies")
+    public void delCookies(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookie.setMaxAge(0);
+                cookie.setPath("/");
+                response.addCookie(cookie);
+            }
+        }
+        try {
+//            response.sendRedirect("https://www.hao123.com/");
+            response.sendRedirect("http://127.0.0.1:9090/swagger-ui.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/addCookies")
+    @ApiOperation("测试addCookies")
+    public void addCookies(HttpServletResponse response) {
+        Cookie newCookie = new Cookie("token", "token_value" + System.currentTimeMillis());
+        newCookie.setMaxAge(10000);
+        newCookie.setPath("/");
+        response.addCookie(newCookie);
+    }
 
     @GetMapping("/testSql")
     @ApiOperation("测试sql")
